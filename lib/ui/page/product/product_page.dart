25527fb4c436bbf18354d29f61a0846d71d81739
Kitavrus <kitavrus@ya.ru>
2021-06-30 16:17:00 +0300

    product page

--- lib/ui/page/product/product_page.dart
+++ lib/ui/page/product/product_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kak_kashka/repository/product_repository.dart';
 
 class ProductPage extends StatefulWidget {
   @override
@@ -6,28 +7,59 @@ class ProductPage extends StatefulWidget {
 }
 
 class _ProductPageState extends State<ProductPage> with AutomaticKeepAliveClientMixin<ProductPage> {
+
+   final _productRepository = ProductRepository();
+   late final _productList;
+
   @override
   void initState() {
     super.initState();
     print('initState Product');
+
+    Future.delayed(Duration.zero, ()
+    {
+      _initProductList();
+      // _productList = await _productRepository.getAll();
+      // print(_productList);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     print('build Product');
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Product'),
-      ),
-      body: Center(
-        child: Text(
-          'This is content of Product',
-          style: TextStyle(fontSize: 30),
+        appBar: AppBar(
+          title: Text('Product'),
         ),
-      ),
+        // body: Text("OK"),
+        body: ProductList(productList: _productList,),
     );
   }
 
   @override
   bool get wantKeepAlive => true;
+
+   void _initProductList() async {
+     _productList = await _productRepository.getAll();
+     print(_productList);
+   }
+
+}
+
+class ProductList extends StatelessWidget {
+  final productList;
+  const ProductList({Key? key,this.productList}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+        itemBuilder: (context, index) {
+          return ListTile(title: Text("# => ${productList[index]}"));
+          // return ListTile(title: Text("# => $index"));
+        },
+        separatorBuilder: (context, index) {
+          return Divider();
+        },
+        itemCount: 3);
+  }
 }
