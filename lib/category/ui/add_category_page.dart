2e2be06cd6cf666c90c7ce37a9f498e3557941b9
Kitavrus <kitavrus@ya.ru>
2021-07-28 17:24:39 +0300

    show category

--- lib/category/ui/add_category_page.dart
+++ lib/category/ui/add_category_page.dart
@@ -2,10 +2,10 @@ import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
+// import 'package:image_picker/image_picker.dart';
 import 'package:kak_kashka/category/model/category_model.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:path/path.dart' as path;
+// import 'package:path_provider/path_provider.dart';
+// import 'package:path/path.dart' as path;
 
 class AddCategoryPage extends StatefulWidget {
   AddCategoryPage({Key? key}) : super(key: key);
@@ -20,12 +20,12 @@ class _AddCategoryPageState extends State<AddCategoryPage> {
   final TextEditingController _descriptionEditingController =
       TextEditingController();
 
-  final TextEditingController _barcodeEditingController =
-      TextEditingController();
+  // final TextEditingController _barcodeEditingController =
+  //     TextEditingController();
 
-  int _radioValue = 0;
-  String appDocPath = '';
-  var _image;
+  // int _radioValue = 0;
+  // String appDocPath = '';
+  // var _image;
 
   @override
   void initState() {
@@ -35,7 +35,7 @@ class _AddCategoryPageState extends State<AddCategoryPage> {
 
   @override
   Widget build(BuildContext context) {
-    print(_image);
+    // print(_image);
 
     return Scaffold(
       appBar: AppBar(
@@ -47,7 +47,7 @@ class _AddCategoryPageState extends State<AddCategoryPage> {
             padding: EdgeInsets.all(10),
             child: Column(
               children: [
-                _selectPhoto(context),
+                // _selectPhoto(context),
                 _textFields(context),
                 ConstrainedBox(
                   constraints: BoxConstraints.tightFor(width: double.infinity),
@@ -59,7 +59,7 @@ class _AddCategoryPageState extends State<AddCategoryPage> {
                       // final newImage = await _image.copy(appDocPath+"/");
                       // final newImage =  await _image.copy(appDocPath+"/4491.jpg");
                       //  Future.delayed(Duration(seconds: 1),() async {
-                      _image.copy(appDocPath + "/4491.jpg").then((newImage) {
+                      // _image.copy(appDocPath + "/4491.jpg").then((newImage) {
                         //   final newImage =  await _image.copy(appDocPath+"/4491.jpg");
                         //   final newImage =  await _image.copy(appDocPath);
                         //   final newImage =  await _image.copy(appDocPath+"/");
@@ -68,18 +68,16 @@ class _AddCategoryPageState extends State<AddCategoryPage> {
 
                         final CategoryModel categoryModel = CategoryModel(
                           id: 4,
-                          status: _radioValue,
+                          status: 1,
                           name: _nameEditingController.text.toString(),
                           description:
                               _descriptionEditingController.text.toString(),
-                          pathToImage: newImage == null
-                              ? 'assets/images/banan.jpg'
-                              : newImage.path,
+                          pathToImage: 'assets/images/banan.jpg',
                         );
 
                         print(categoryModel);
                         Navigator.pop(context, categoryModel);
-                      });
+                      // });
                     },
                   ),
                 ),
@@ -127,99 +125,54 @@ class _AddCategoryPageState extends State<AddCategoryPage> {
               onChanged: (value) {},
             ),
           ),
-          // SizedBox(height: 5),
-          // Container(
-          //   child: TextField(
-          //     controller: _barcodeEditingController,
-          //     maxLength: 14,
-          //     textAlign: TextAlign.start,
-          //     keyboardType: TextInputType.phone,
-          //     decoration: InputDecoration(
-          //       labelText: "Штрих-код товара",
-          //       border: OutlineInputBorder(),
-          //       contentPadding: const EdgeInsets.all(5),
-          //       counterText: '',
-          //     ),
-          //     onChanged: (value) {},
-          //   ),
-          // ),
-          // Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-          //   Row(
-          //     mainAxisAlignment: MainAxisAlignment.start,
-          //     children: [
-          //       Radio(
-          //         value: 3,
-          //         groupValue: _radioValue,
-          //         onChanged: _handleRadioValueChange,
-          //       ),
-          //       Text("покупать"),
-          //     ],
-          //   ),
-          //   Row(mainAxisAlignment: MainAxisAlignment.start, children: [
-          //     Radio(
-          //       value: 2,
-          //       groupValue: _radioValue,
-          //       onChanged: _handleRadioValueChange,
-          //     ),
-          //     Text("иногда"),
-          //   ]),
-          //   Row(mainAxisAlignment: MainAxisAlignment.start, children: [
-          //     Radio(
-          //       value: 1,
-          //       groupValue: _radioValue,
-          //       onChanged: _handleRadioValueChange,
-          //     ),
-          //     Text("не покупать"),
-          //   ]),
-          // ]),
         ],
       ),
     );
   }
 
-  Widget _selectPhoto(BuildContext context) {
-    return GestureDetector(
-      onTap: () {_showPicker(context); },
-      child: Container(
-        width: MediaQuery.of(context).size.width,
-        height: 250,
-        decoration: ShapeDecoration(
-          color: Colors.grey[200],
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(10)),
-          ),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Icon(Icons.camera_alt),
-                SizedBox(width: 5,),
-                Text("Добавить фото товара")
-              ],
-            ),
-            _image == null
-                ? SizedBox()
-                : SizedBox(
-                    width: MediaQuery.of(context).size.width,
-                    height: 200,
-                    child: Image.file(_image))
-          ],
-        ),
-      ),
-    );
-  }
+  // Widget _selectPhoto(BuildContext context) {
+  //   return GestureDetector(
+  //     onTap: () {_showPicker(context); },
+  //     child: Container(
+  //       width: MediaQuery.of(context).size.width,
+  //       height: 250,
+  //       decoration: ShapeDecoration(
+  //         color: Colors.grey[200],
+  //         shape: RoundedRectangleBorder(
+  //           borderRadius: BorderRadius.all(Radius.circular(10)),
+  //         ),
+  //       ),
+  //       child: Column(
+  //         mainAxisAlignment: MainAxisAlignment.center,
+  //         crossAxisAlignment: CrossAxisAlignment.center,
+  //         children: [
+  //           Row(
+  //             mainAxisAlignment: MainAxisAlignment.center,
+  //             crossAxisAlignment: CrossAxisAlignment.center,
+  //             children: [
+  //               Icon(Icons.camera_alt),
+  //               SizedBox(width: 5,),
+  //               Text("Добавить фото товара")
+  //             ],
+  //           ),
+  //           _image == null
+  //               ? SizedBox()
+  //               : SizedBox(
+  //                   width: MediaQuery.of(context).size.width,
+  //                   height: 200,
+  //                   child: Image.file(_image))
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  // }
 
-  void _handleRadioValueChange(int? value) {
-    setState(() {
-      _radioValue = value!;
-      print(_radioValue);
-    });
-  }
+  // void _handleRadioValueChange(int? value) {
+  //   setState(() {
+  //     _radioValue = value!;
+  //     print(_radioValue);
+  //   });
+  // }
 
   // Future<File> moveFile(File sourceFile, String newPath) async {
   //   try {
@@ -235,61 +188,61 @@ class _AddCategoryPageState extends State<AddCategoryPage> {
   // }
 
   void initPath() async {
-    Directory appDocDir = await getApplicationDocumentsDirectory();
-    appDocPath = appDocDir.path;
+    // Directory appDocDir = await getApplicationDocumentsDirectory();
+    // appDocPath = appDocDir.path;
   }
 
-  _imageFromCamera() async {
-    final image = await ImagePicker()
-        .getImage(source: ImageSource.camera, imageQuality: 50);
+  // _imageFromCamera() async {
+  //   final image = await ImagePicker()
+  //       .getImage(source: ImageSource.camera, imageQuality: 50);
+  //
+  //   if (image != null) {
+  //     setState(() {
+  //       _image = File(image.path);
+  //
+  //       // File tmp = File(image.path);
+  //       // _image = await tmp.copy(appDocPath+"/_x.jpg");
+  //     });
+  //   }
+  // }
 
-    if (image != null) {
-      setState(() {
-        _image = File(image.path);
+  // _imageFromGallery() async {
+  //   final image = await ImagePicker()
+  //       .getImage(source: ImageSource.gallery, imageQuality: 50);
+  //   if (image != null) {
+  //     setState(() {
+  //       _image = File(image.path);
+  //     });
+  //   }
+  // }
 
-        // File tmp = File(image.path);
-        // _image = await tmp.copy(appDocPath+"/_x.jpg");
-      });
-    }
-  }
-
-  _imageFromGallery() async {
-    final image = await ImagePicker()
-        .getImage(source: ImageSource.gallery, imageQuality: 50);
-    if (image != null) {
-      setState(() {
-        _image = File(image.path);
-      });
-    }
-  }
-
-  void _showPicker(context) {
-    showModalBottomSheet(
-      context: context,
-      builder: (BuildContext context) {
-        return SafeArea(
-          child: Wrap(
-            children: [
-              ListTile(
-                leading: Icon(Icons.folder),
-                title: Text("Gallery "),
-                onTap: () {
-                  _imageFromGallery();
-                  Navigator.of(context).pop();
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.camera_alt),
-                title: Text("Camera "),
-                onTap: () {
-                  _imageFromCamera();
-                  Navigator.of(context).pop();
-                },
-              ),
-            ],
-          ),
-        );
-      },
-    );
-  }
+  // void _showPicker(context) {
+  //   showModalBottomSheet(
+  //     context: context,
+  //     builder: (BuildContext context) {
+  //       return SafeArea(
+  //         child: Wrap(
+  //           children: [
+  //             ListTile(
+  //               leading: Icon(Icons.folder),
+  //               title: Text("Gallery "),
+  //               onTap: () {
+  //                 _imageFromGallery();
+  //                 Navigator.of(context).pop();
+  //               },
+  //             ),
+  //             ListTile(
+  //               leading: Icon(Icons.camera_alt),
+  //               title: Text("Camera "),
+  //               onTap: () {
+  //                 _imageFromCamera();
+  //                 Navigator.of(context).pop();
+  //               },
+  //             ),
+  //           ],
+  //         ),
+  //       );
+  //     },
+  //   );
+  // }
 }
--- lib/category/ui/category_detail_page.dart
+++ lib/category/ui/category_detail_page.dart
@@ -1,55 +1,21 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kak_kashka/category/entity/category_entity.dart';
+
+import 'package:kak_kashka/product/cubit/product_cubit.dart';
+import 'package:kak_kashka/product/cubit/product_state.dart';
+import 'package:kak_kashka/product/entity/product_entity.dart';
+import 'package:kak_kashka/product/repository/product_repository.dart';
+import 'package:kak_kashka/product/ui/product_detail_page.dart';
+import 'package:path/path.dart' as path;
 
 class CategoryDetail extends StatelessWidget {
   final CategoryEntity categoryModel;
 
-  const CategoryDetail({Key? key, required this.categoryModel}) : super(key: key);
-
-  // Future<void> _displayDialog(
-  //     BuildContext context, CategoryEntity categoryModel) {
-  //   return showGeneralDialog(
-  //     context: context,
-  //     barrierDismissible: false,
-  //     transitionDuration: Duration(milliseconds: 300),
-  //     transitionBuilder: (context, animation, secondaryAnimation, child) {
-  //       return FadeTransition(
-  //         opacity: animation,
-  //         child: ScaleTransition(
-  //           scale: animation,
-  //           child: child,
-  //         ),
-  //       );
-  //     },
-  //     pageBuilder: (context, animation, secondaryAnimation) {
-  //       return SafeArea(
-  //         child: Stack(
-  //           children: [
-  //             Container(
-  //               width: MediaQuery.of(context).size.width,
-  //               height: MediaQuery.of(context).size.height,
-  //               padding: EdgeInsets.all(5),
-  //               color: Colors.black45,
-  //               child: Image.asset(
-  //                 categoryModel.pathToImage,
-  //                 // fit: BoxFit.cover,
-  //               ),
-  //             ),
-  //             GestureDetector(
-  //               child: Icon(
-  //                 Icons.close,
-  //                 color: Colors.white,
-  //               ),
-  //               onTap: () {
-  //                 Navigator.of(context).pop();
-  //               },
-  //             ),
-  //           ],
-  //         ),
-  //       );
-  //     },
-  //   );
-  // }
+  const CategoryDetail({Key? key, required this.categoryModel})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -64,103 +30,238 @@ class CategoryDetail extends StatelessWidget {
       ),
       body: Stack(
         children: [
-          SingleChildScrollView(
-            child: Column(
-              children: [
-                Stack(
-                  children: [
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.5,
-                      width: MediaQuery.of(context).size.width,
-                      child: Text(categoryModel.name),
-                      // Hero(
-                      //   tag: 'prod-image' + categoryModel.id.toString(),
-                      //   child:
-                        // GestureDetector(
-                        //   onTap: () {
-                        //     print("GestureDetector onTap");
-                        //     _displayDialog(context, categoryModel);
-                        //   },
-                        //   child: Image.asset(
-                        //     categoryModel.pathToImage,
-                        //     fit: BoxFit.cover,
-                        //   ),
-                        // ),
-                      // ),
-                      // child:
-                    ),
-                    // Container(
-                    //   height: MediaQuery.of(context).size.height * 0.5,
-                    //   width: MediaQuery.of(context).size.width,
-                    //   decoration: BoxDecoration(
-                    //     gradient: LinearGradient(
-                    //       begin: Alignment.topCenter,
-                    //       end: Alignment.bottomCenter,
-                    //       stops: [0.0, 0.4],
-                    //       colors: [
-                    //         Colors.black.withOpacity(0.9),
-                    //         Colors.black.withOpacity(0.0),
-                    //       ],
-                    //     ),
-                    //   ),
-                    // ),
-                    Column(
-                      children: [
-                        SizedBox(
-                          height: MediaQuery.of(context).size.height * 0.46,
-                        ),
-                        Container(
-                          height: 500,
-                          width: double.infinity,
-                          padding: EdgeInsets.symmetric(vertical: 30),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.only(
-                                    topLeft: Radius.circular(40),
-                                    topRight: Radius.circular(40),
-                            ),
-                            color: Colors.white,
-                          ),
-
-                          // decoration: ShapeDecoration(
-                          //   color: Colors.white,
-                          //   shape: RoundedRectangleBorder(
-                          //     borderRadius: BorderRadius.only(
-                          //       topLeft: Radius.circular(40),
-                          //       topRight: Radius.circular(40),
-                          //     ),
-                          //   ),
-                          // ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(horizontal: 20),
-                                child: Text(
-                                  categoryModel.name,
-                                  style: Theme.of(context).textTheme.headline5,
-                                ),
-                              ),
-                              Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(horizontal: 20),
-                                child: Text(
-                                  categoryModel.description,
-                                  style: Theme.of(context).textTheme.subtitle2,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Text(
+                  categoryModel.name,
+                  style: Theme.of(context).textTheme.headline5,
                 ),
-              ],
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Text(
+                  categoryModel.description,
+                  style: Theme.of(context).textTheme.subtitle2,
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 80),
+            child: Container(
+              height: MediaQuery.of(context).size.height,
+              width: double.infinity,
+              padding: EdgeInsets.symmetric(vertical: 30, horizontal: 5),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(40),
+                  topRight: Radius.circular(40),
+                ),
+                color: Colors.black12,
+              ),
+              child: ProductCategoryPage(),
             ),
           ),
         ],
       ),
     );
+  }
+}
+
+class ProductCategoryPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    print('build Product');
+    return MultiBlocProvider(providers: [
+      BlocProvider<ProductCubit>(
+        create: (context) =>
+            ProductCubit(ProductRepository())..getProductList(),
+      ),
+    ], child: ProductPageView());
+  }
+}
+
+class ProductPageView extends StatelessWidget {
+  const ProductPageView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ProductCubit, ProductState>(
+      builder: (context, state) {
+        List<ProductEntity> productList = [];
+
+        print("BlocBuilder: ");
+        print(state.status);
+        switch (state.status) {
+          case ProductStatus.initial:
+            return _loadingIndicator();
+          case ProductStatus.loading:
+            return _loadingIndicator();
+          case ProductStatus.failure:
+            return _showError(state.message);
+
+          case ProductStatus.success:
+            productList = state.productList;
+
+            return Stack(
+              children: [
+                _showSearchBar(context),
+                Padding(
+                  padding: const EdgeInsets.only(top: 70),
+                  child: ProductList(productList: productList),
+                ),
+              ],
+            );
+          default:
+            return _showEmpty("NO data");
+        }
+      },
+    );
+  }
+
+  Widget _showSearchBar(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextField(
+        decoration: InputDecoration(
+          labelText: "Поиск ...",
+          // filled: true,
+        ),
+        onChanged: (text) {
+          context.read<ProductCubit>().searchProduct(text);
+        },
+      ),
+    );
+  }
+
+  Widget _loadingIndicator() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+
+  Widget _showError(message) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Center(
+        child: Text(
+          message,
+          style: TextStyle(color: Colors.black, fontSize: 25),
+        ),
+      ),
+    );
+  }
+
+  Widget _showEmpty(message) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Center(
+        child: Text(
+          message,
+          style: TextStyle(color: Colors.black, fontSize: 25),
+        ),
+      ),
+    );
+  }
+}
+
+class ProductList extends StatelessWidget {
+  final List<ProductEntity> productList;
+
+  const ProductList({Key? key, required this.productList}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    print("build: ProductList ");
+
+    return ListView.separated(
+      keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+      itemCount: productList.length,
+      itemBuilder: (context, index) {
+        final product = productList[index];
+        return ProductCard(
+          productModel: product,
+        );
+      },
+      separatorBuilder: (context, index) => Divider(),
+    );
+  }
+}
+
+class ProductCard extends StatelessWidget {
+  final ProductEntity productModel;
+
+  const ProductCard({Key? key, required this.productModel}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+        colors: [Colors.white, _colorByStatus(productModel)],
+      )),
+      child: ListTile(
+        leading: _getImage(productModel),
+        title: Text(productModel.name),
+        subtitle: Text(productModel.description),
+        trailing: Icon(Icons.chevron_right),
+        onTap: () {
+          print("onTap: " + productModel.name);
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => ProductDetail(productModel: productModel),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Color _colorByStatus(ProductEntity productModel) {
+    final _color;
+
+    switch (productModel.status) {
+      case 0:
+        _color = Colors.white;
+        break;
+      case 1:
+        _color = Colors.red[200];
+        break;
+      case 2:
+        _color = Colors.yellow[200];
+        break;
+      case 3:
+        _color = Colors.green[200];
+        break;
+      default:
+        _color = Colors.white;
+    }
+    return _color;
+  }
+
+  Widget _getImage(ProductEntity productModel) {
+    return SizedBox(
+      child: _imagePathType(productModel.pathToImage) == "assets"
+          ? Image.asset(productModel.pathToImage)
+          : Image.file(File(productModel.pathToImage)),
+      width: 50,
+      height: 50,
+    );
+  }
+
+  String _imagePathType(String pathToImage) {
+    String pathToImage = productModel.pathToImage;
+    List<String> splitPath = path.split(pathToImage);
+    if (splitPath.first == "assets") {
+      return 'assets';
+    }
+    return 'file';
   }
 }
